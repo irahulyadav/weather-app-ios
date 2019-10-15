de7b3fb90e4979be523854da86d2ff8faedcf2bc
Rahul Yadav <rahul.yadav@indpro.se>
2019-10-15 08:43:54 +0200

    Initial Commit

---
+++ Weather App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by Rahul on 2019-10-15.
+//  Copyright © 2019 AppsZum. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
